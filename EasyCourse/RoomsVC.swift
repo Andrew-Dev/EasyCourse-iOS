cd03fbb302a498a96efcf996ab2e417f3be49c87
andrew <andrew@andrewarpasi.com>
2017-01-14 15:52:06 -0500

    Search Courses

--- EasyCourse/RoomsVC.swift
+++ EasyCourse/RoomsVC.swift
@@ -25,6 +25,9 @@ class RoomsVC: UIViewController {
     var sortedRooms:[(room:Room,lastMessage:Message?)] = []
     var roomUpdateNotif: NotificationToken? = nil
     var messageUpdateNotif: NotificationToken? = nil
+    let searchBar = UISearchBar()
+    let searchResultsTableView = UITableView()
+    var searchResults: [Course] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -50,6 +53,10 @@ class RoomsVC: UIViewController {
             self.roomTableView.reloadData()
         })
         
+        searchBar.placeholder = "Search Courses"
+        searchBar.delegate = self
+        self.navigationItem.titleView = searchBar
+        searchResultsTableView.register(UINib(nibName: "CourseTVCell", bundle: nil), forCellReuseIdentifier: "UserCoursesTVCell")
     }
     
     deinit {
@@ -146,21 +153,77 @@ class RoomsVC: UIViewController {
     func showAddRoom() {
         self.performSegue(withIdentifier: "showAddRoom", sender: self)
     }
+    
+    func doneSearching() {
+        searchBar.resignFirstResponder()
+        searchBar.text = ""
+        searchResults = []
+        searchResultsTableView.reloadData()
+        let addRoomBtn = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(self.showAddRoom))
+        self.navigationItem.rightBarButtonItem = addRoomBtn
+        searchResultsTableView.removeFromSuperview()
+    }
+
 }
 
 extension RoomsVC: UITableViewDelegate, UITableViewDataSource {
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if tableView == searchResultsTableView {
+            return searchResults.count
+        }
         return sortedRooms.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if tableView == searchResultsTableView {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "UserCoursesTVCell", for: indexPath) as! UserCoursesTVCell
+            cell.configureCell(searchResults[indexPath.row], userJoinedCourses: User.currentUser!.joinedCourse)
+            return cell
+        }
         let cell = tableView.dequeueReusableCell(withIdentifier: "RoomsTVCell", for: indexPath) as! RoomsTVCell
         cell.configureCell(sortedRooms[(indexPath as NSIndexPath).row].0, lastMessage: sortedRooms[(indexPath as NSIndexPath).row].1)
         return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        self.performSegue(withIdentifier: "gotoLocalRoom", sender: self)
+        if tableView == searchResultsTableView {
+            let storyboard = UIStoryboard(name: "User", bundle: nil)
+            let courseDetailVC = storyboard.instantiateViewController(withIdentifier: "CourseDetailVC") as! CourseDetailVC
+            if let cell = tableView.cellForRow(at: indexPath) as? UserCoursesTVCell {
+                courseDetailVC.courseId = cell.cellCourse?.id
+                self.navigationController?.pushViewController(courseDetailVC, animated: true)
+            }
+
+        } else {
+            self.performSegue(withIdentifier: "gotoLocalRoom", sender: self)
+        }
         tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
+extension RoomsVC: UISearchBarDelegate {
+    
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        let doneBtn = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(self.doneSearching))
+        navigationItem.rightBarButtonItem = doneBtn
+        searchResultsTableView.frame = roomTableView.frame
+        searchResultsTableView.delegate = self
+        searchResultsTableView.dataSource = self
+        searchResultsTableView.rowHeight = 65
+        self.view.addSubview(searchResultsTableView)
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        SocketIOManager.sharedInstance.searchCourse(searchText, universityId: User.currentUser!.universityId!, limit: 15, skip: 0, completion: { (courseArr, error) in
+            if error == nil {
+                self.searchResults = courseArr
+                self.searchResultsTableView.reloadData()
+            }
+        })
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
     }
 }
